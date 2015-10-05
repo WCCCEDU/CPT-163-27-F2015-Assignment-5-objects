c481d65db9030970f80b1d21d552e923a9d9bb9c
payneda <payneda@my.wccc.edu>
2015-10-05 18:18:21 -0400

    Daniel Payne assignment 5
    Assignment 5.1 and Car.java
    Assignment 5.2 and ReatailItem,java
    Sorry in advanced.

---
+++ Assignment5.2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment5.pkg3;
+
+/**
+ *
+ * @author Home pc
+ */
+public class Assignment53 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Creats a new object RealtailItem and Enters feilds manually.
+        ReatailItem newItem = new ReatailItem("",0,0.0);
+        newItem.enterProduct();
+    }
+    
+}
